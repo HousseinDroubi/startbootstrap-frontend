6a19b06d0deec65884281e76e51263b78e44a503
HousseinDroubi <housseinalialdroubi@gmail.com>
2022-09-14 11:01:48 +0300

    Fetch messages from form table and implement them using appendChild and for loop

--- scripts/view_messages_scripts.js
+++ scripts/view_messages_scripts.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+const view_messages_paragraph=document.getElementById("view_messages_paragraph");
+// Fetch messages from DataBase
+let url = "http://localhost/startbootstrap-frontend/php/getmessages.php";
+
+        fetch(url)
+        .then(respone=>respone.json())
+        .then(data=>{
+            console.log(data);
+            for(let i=0;i<data.length;i++){
+                let p = document.createElement('p');
+                p.classList.add("message");
+                p.innerText="test";
+                view_messages_paragraph.appendChild(p);
+            }
+        });
