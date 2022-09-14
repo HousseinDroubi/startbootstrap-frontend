b34ac411d0a57996c7703dcaee400d5e388bd28c
HousseinDroubi <housseinalialdroubi@gmail.com>
2022-09-14 09:44:56 +0300

    Add some comments

--- php/addform.php
+++ php/addform.php
@@ -3,17 +3,16 @@ include("connection.php");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-
-
+// Get all parameters using POST method
 $full_name=$_POST['full_name'];
 $email=$_POST['email'];
 $phone_number=$_POST['phone_number'];
 $message=$_POST['message'];
-
+// Insert parameters into form table
 $query=$mysqli->prepare("INSERT INTO form(full_name,email,phone_number,message) values(?,?,?,?)");
 $query->bind_param("ssss",$full_name,$email,$phone_number,$message);
 $query->execute();
-
+// Return response as JSON
 $response=[];
 $response["success"]=true;
 echo json_encode($response);
--- php/getmessages.php
+++ php/getmessages.php
@@ -3,11 +3,11 @@ include("connection.php");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-
+// get all messages
 $query=$mysqli->prepare("SELECT message from form");
 $query->execute();
 $array=$query->get_result();
-
+// Create Json response
 $response=[];
 while($a = $array->fetch_assoc()){
     $response[]= $a;
